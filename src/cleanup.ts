95cff599be5738875f1d9c2e646e71be69278ded
YanaXu <sheshouxy@gmail.com>
2024-09-11 16:06:38 +0800

    update cleanup warning message

--- src/cleanup.ts
+++ src/cleanup.ts
@@ -1,16 +1,20 @@
 import * as core from '@actions/core';
-import { setUserAgent, cleanupAzCLIAccounts, cleanupAzPSAccounts } from './common/Utils'; 
+import { setUserAgent, cleanupAzCLIAccounts, cleanupAzPSAccounts } from './common/Utils';
 
 async function cleanup() {
     try {
         setUserAgent();
         await cleanupAzCLIAccounts();
-        if(core.getInput('enable-AzPSSession').toLowerCase() === "true"){
+        if (core.getInput('enable-AzPSSession').toLowerCase() === "true") {
             await cleanupAzPSAccounts();
         }
     }
     catch (error) {
-        core.warning(`Login cleanup failed with ${error}. Cleanup will be skipped.`);
+        if (error && error.message) {
+            core.warning(`Login cleanup failed due to the reason: '${error.message}'. Cleanup will be skipped. If the cleanup step should NOT be run, please set environment variable 'AZURE_LOGIN_SKIP_PRE' or 'AZURE_LOGIN_SKIP_POST' to true.`); 
+        } else { 
+            core.warning(`Login cleanup failed due to the reason: '${error}'. Cleanup will be skipped. If the cleanup step should NOT be run, please set environment variable 'AZURE_LOGIN_SKIP_PRE' or 'AZURE_LOGIN_SKIP_POST' to true.`); 
+        }
         core.debug(error.stack);
     }
 }
