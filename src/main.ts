7eb759eb2675aaeb9ed3a0343475d74d3e5c24be
YanaXu <sheshouxy@gmail.com>
2024-09-12 11:26:14 +0800

    move cleanup to main

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,28 @@
 import * as core from '@actions/core';
-import { setUserAgent } from './common/Utils'; 
+import { cleanupAzCLIAccounts, cleanupAzPSAccounts, setUserAgent } from './common/Utils'; 
 import { AzPSLogin } from './PowerShell/AzPSLogin';
 import { LoginConfig } from './common/LoginConfig';
 import { AzureCliLogin } from './Cli/AzureCliLogin';
 
 async function main() {
+    core.warning("I'm in main!");
     try {
         setUserAgent();
-
+        await cleanupAzCLIAccounts();
+        if (core.getInput('enable-AzPSSession').toLowerCase() === "true") {
+            await cleanupAzPSAccounts();
+        }
+    }
+    catch (error) {
+        if (error && error.message) {
+            core.warning(`Login cleanup failed due to the reason: '${error.message}'. Cleanup will be skipped. If the cleanup step should NOT be run, please set environment variable 'AZURE_LOGIN_SKIP_PRE' or 'AZURE_LOGIN_SKIP_POST' to true.`); 
+        } else { 
+            core.warning(`Login cleanup failed due to the reason: '${error}'. Cleanup will be skipped. If the cleanup step should NOT be run, please set environment variable 'AZURE_LOGIN_SKIP_PRE' or 'AZURE_LOGIN_SKIP_POST' to true.`); 
+        }
+        core.debug(error.stack);
+    }
+    
+    try {
         // prepare the login configuration
         var loginConfig = new LoginConfig();
         await loginConfig.initialize();
